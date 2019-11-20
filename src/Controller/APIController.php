5ff6ffc59b65662c29615e7722ee4804aec90d87
Álvaro Saúl Rodríguez Alemán <rodriguezalemanalvaro@gmail.com>
2019-11-20 19:52:09 +0000

    añadidos algunos metodos de la api

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class APIController extends AbstractController
 {
     /**
-     * @Route("/api/getUser/{user}", name="getUser", methods={"GET"})
+     * @Route("/api/getUser/{user}", name="getUser")
      * @param string $user
      * @param DataAccess $dataAccess
      * @return Response
@@ -23,25 +23,227 @@ class APIController extends AbstractController
     }
 
     /**
-     * @Route("/api/getPregunta/{pregunta}", name="getPregunta", methods={"GET"})
-     * @param string $pregunta
+     * @Route("/api/getPregunta/{id}", name="getPregunta")
+     * @param string $id
      * @param DataAccess $dataAccess
      * @return Response
      */
-    public function getPregunta(string $pregunta, DataAccess $dataAccess)
+    public function getPregunta(string $id, DataAccess $dataAccess)
     {
-        return new JsonResponse($dataAccess->getPregunta($pregunta));
+        return new JsonResponse($dataAccess->getPregunta($id));
+    }
+
+    /**
+     * @Route("/api/getPreguntasFromUser/{user}", name="getPreguntasFromUser")
+     * @param string $user
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getPreguntasFromUser(string $user, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getPreguntasFromUser($user));
+    }
+
+    /**
+     * @Route("/api/getEncuesta/{id}", name="getEncuesta")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getEncuesta(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getEncuesta($id));
+    }
+
+    /**
+     * @Route("/api/getEncuestasFromUser/{id}", name="getEncuestasFromUser")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getEncuestasFromUser(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getEncuestasFromUser($id));
+    }
+
+    /**
+     * @Route("/api/getPreguntaMultiopcion/{id}", name="getPreguntaMultiopcion")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getPreguntaMultiopcion(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getPreguntaMultiopcion($id));
+    }
+
+    /**
+     * @Route("/api/getPreguntasMultiopcionFromUser/{id}", name="getPreguntasMultiopcionFromUser")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getPreguntasMultiopcionFromUser(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getPreguntasMultiopcionFromUser($id));
+    }
+
+    /**
+     * @Route("/api/getSiguiendo/{user}", name="getSiguiendo")
+     * @param string $user
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getSiguiendo(string $user, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getSiguiendo($user));
+    }
+
+    /**
+     * @Route("/api/getSeguidores/{user}", name="getSeguidores")
+     * @param string $user
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getSeguidores(string $user, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getSeguidores($user));
+    }
+
+    /**
+     * @Route("/api/getRespuesta/{id}", name="getRespuesta")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getRespuesta(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getRespuesta($id));
+    }
+
+    /**
+     * @Route("/api/getRespuestasFromPregunta/{id}", name="getRespuestasFromPregunta")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getRespuestasFromPregunta(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getRespuestasFromPregunta($id));
+    }
+
+    /**
+     * @Route("/api/getRespuestasFromUsuario/{id}", name="getRespuestasFromUsuario")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getRespuestasFromUsuario(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getRespuestasFromUsuario($id));
+    }
+
+    /**
+     * @Route("/api/getOpcion/{id}", name="getOpcion")
+     * @param string $id
+     * @param DataAccess $dataAccess
+     * @return Response
+     */
+    public function getOpcion(string $id, DataAccess $dataAccess)
+    {
+        return new JsonResponse($dataAccess->getOpcion($id));
     }
 
     /**
      * @Route("/api/createUser", name="createUser")
      * @param DataAccess $dataAccess
+     * @param Request $request
      * @return string
      */
     public function createUser(DataAccess $dataAccess, Request $request)
     {
         if (!empty(json_decode($request->getContent(), true)["USERNAME"])) {
             $dataAccess->createUser(json_decode($request->getContent(), true));
+            return new JsonResponse(true);
+        } else {
+            return new JsonResponse(false);
+        }
+    }
+
+    /**
+     * @Route("/api/createPregunta", name="createPregunta")
+     * @param DataAccess $dataAccess
+     * @param Request $request
+     * @return string
+     */
+    public function createPregunta(DataAccess $dataAccess, Request $request)
+    {
+        if (!empty(json_decode($request->getContent(), true)["TEXT"])) {
+            $dataAccess->createPregunta(json_decode($request->getContent(), true));
+            return new JsonResponse(true);
+        } else {
+            return new JsonResponse(false);
+        }
+    }
+
+    /**
+     * @Route("/api/createEncuesta", name="createEncuesta")
+     * @param DataAccess $dataAccess
+     * @param Request $request
+     * @return string
+     */
+    public function createEncuesta(DataAccess $dataAccess, Request $request)
+    {
+        if (!empty(json_decode($request->getContent(), true)["CONTENIDO"])) {
+            $dataAccess->createEncuesta(json_decode($request->getContent(), true));
+            return new JsonResponse(true);
+        } else {
+            return new JsonResponse(false);
+        }
+    }
+
+    /**
+     * @Route("/api/createPreguntaMultiopcion", name="createPreguntaMultiopcion")
+     * @param DataAccess $dataAccess
+     * @param Request $request
+     * @return string
+     */
+    public function createPreguntaMultiopcion(DataAccess $dataAccess, Request $request)
+    {
+        if (!empty(json_decode($request->getContent(), true)["CONTENIDO"])) {
+            $dataAccess->createPreguntaMultiopcion(json_decode($request->getContent(), true));
+            return new JsonResponse(true);
+        } else {
+            return new JsonResponse(false);
+        }
+    }
+
+    /**
+     * @Route("/api/createRespuesta", name="createRespuesta")
+     * @param DataAccess $dataAccess
+     * @param Request $request
+     * @return string
+     */
+    public function createRespuesta(DataAccess $dataAccess, Request $request)
+    {
+        if (!empty(json_decode($request->getContent(), true)["CONTENIDO"])) {
+            $dataAccess->createRespuesta(json_decode($request->getContent(), true));
+            return new JsonResponse(true);
+        } else {
+            return new JsonResponse(false);
+        }
+    }
+
+    /**
+     * @Route("/api/followUser", name="followUser")
+     * @param DataAccess $dataAccess
+     * @param Request $request
+     * @return string
+     */
+    public function followUser(DataAccess $dataAccess, Request $request)
+    {
+        if (!empty(json_decode($request->getContent(), true)["USUARIO_QUE_SIGUE"])) {
+            $dataAccess->followUser(json_decode($request->getContent(), true));
             return new JsonResponse(true);
         } else {
             return new JsonResponse(false);
--- src/Service/DataAccess.php
+++ src/Service/DataAccess.php
@@ -4,17 +4,104 @@ namespace App\Service;
 
 class DataAccess extends BaseDataAccess {
     public function getUser(string $user) {
-        return parent::executeSQL("SELECT * FROM USUARIO WHERE USERNAME = :user;", ["user" => $user])->fetch();
+        return parent::executeSQL("SELECT * FROM usuario WHERE USERNAME = :user;", ["user" => $user])->fetch();
     }
 
     public function getPregunta(string $id) {
-        return parent::executeSQL("SELECT * FROM PREGUNTA WHERE ID = :id", ["id" => $id])->fetch();
+        return parent::executeSQL("SELECT * FROM pregunta WHERE ID = :id", ["id" => $id])->fetch();
+    }
+
+    public function getPreguntasFromUser(string $id) {
+        return parent::executeSQL("SELECT * FROM pregunta WHERE USERID = :id", ["id" => $id])->fetchAll();
+    }
+
+    public function getEncuesta(string $id) {
+        return parent::executeSQL("SELECT * FROM pregunta_encuesta WHERE ID = :id", ["id" => $id])->fetch();
+    }
+
+    public function getEncuestasFromUser(string $id) {
+        return parent::executeSQL("SELECT * FROM pregunta_encuesta WHERE USERID = :id", ["id" => $id])->fetchAll();
+    }
+
+    public function getPreguntaMultiopcion(string $id) {
+        return parent::executeSQL("SELECT * FROM pregunta_opciones WHERE ID = :id", ["id" => $id])->fetch();
+    }
+
+    public function getPreguntasMultiopcionFromUser(string $id) {
+        return parent::executeSQL("SELECT * FROM pregunta_opciones WHERE USERID = :id", ["id" => $id])->fetchAll();
+    }
+
+    public function getSiguiendo(string $username) {
+        return parent::executeSQL("SELECT USUARIO_SEGUIDO FROM seguidores WHERE USUARIO_QUE_SIGUE = :username",
+            ["username" => $username])->fetchAll();
+    }
+
+    public function getSeguidores(string $username) {
+        return parent::executeSQL("SELECT USUARIO_QUE_SIGUE FROM seguidores WHERE USUARIO_SEGUIDO = :username",
+            ["username" => $username])->fetchAll();
+    }
+
+    public function getRespuesta(string $id) {
+        return parent::executeSQL("SELECT * FROM respuesta WHERE ID = :id", ["id" => $id])->fetch();
+    }
+
+    public function getRespuestasFromPregunta(string $id) {
+        return parent::executeSQL("SELECT * FROM respuesta WHERE PREGUNTAID = :id", ["id" => $id])->fetchAll();
+    }
+
+    public function getRespuestasFromUsuario(string $id) {
+        return parent::executeSQL("SELECT * FROM respuesta WHERE USERID = :id", ["id" => $id])->fetchAll();
+    }
+
+    public function getOpcion(string $id) {
+        return parent::executeSQL("SELECT * FROM opcion WHERE ID = :id", ["id" => $id])->fetch();
     }
 
     public function createUser(array $data) {
-        return parent::executeSQL("INSERT INTO USUARIO(USERNAME, NAME, PASSWORD, EMAIL, COUNTRY, PROVINCE, ROLE) 
+        return parent::executeSQL("INSERT INTO usuario(USERNAME, NAME, PASSWORD, EMAIL, COUNTRY, PROVINCE, ROLE) 
                                     VALUES (:username, :nombre, :pass, :email, :country, :province, 1);",
                                 ["username" => $data["USERNAME"], "nombre" => $data["NAME"], "pass" => $data["PASSWORD"],
                                     "email" => $data["EMAIL"], "country" => $data["COUNTRY"], "province" => $data["PROVINCE"]]);
+    }
+
+    public function createPregunta(array $data) {
+        return parent::executeSQL("INSERT INTO pregunta(TEXT, USERID, REPORTS, LIKES, ANONYMOUS) 
+                                    VALUES (:TEXT_, :USERID, :REPORTS, :LIKES, :ANONYMOUS);",
+            ["TEXT_" => $data["TEXT"], "USERID" => $data["USERID"], "REPORTS" => $data["REPORTS"],
+                "LIKES" => $data["LIKES"], "ANONYMOUS" => $data["ANONYMOUS"]]);
+    }
+
+    private function createOpcion($enunciado, $respuesta_correcta) {
+        parent::executeSQL("INSERT INTO opcion(CONTENIDO, OPCION_CORRECTA, VECES_SELECCIONADAS) 
+                                    VALUES (:CONTENIDO, :OPCION_CORRECTA, :VECES_SELECCIONADAS);",
+            ["CONTENIDO" => $enunciado, "OPCION_CORRECTA" => $respuesta_correcta, "VECES_SELECCIONADAS" => 0]);
+        return parent::getlastInsertId();
+    }
+
+    public function createEncuesta(array $data) {
+        parent::executeSQL("INSERT INTO pregunta_encuesta(CONTENIDO, OPCION_UNO, OPCION_DOS, USERID)
+                                    VALUES (:CONTENIDO, :OPCION_UNO, :OPCION_DOS, :USERID);",
+            ["CONTENIDO" => $data["CONTENIDO"], "OPCION_UNO" => $this->createOpcion($data["OPCION_UNO"], ($data["RESPUESTA_CORRECTA"] == "1") ? 1 : 0),
+                "OPCION_DOS" => $this->createOpcion($data["OPCION_DOS"], ($data["RESPUESTA_CORRECTA"] == "2") ? 1 : 0), "USERID" => $data["USERID"]]);
+    }
+
+    public function createPreguntaMultiopcion(array $data) {
+        parent::executeSQL("INSERT INTO pregunta_opciones(CONTENIDO, OPCION_UNO, OPCION_DOS, OPCION_TRES, USERID)
+                                    VALUES (:CONTENIDO, :OPCION_UNO, :OPCION_DOS, :OPCION_TRES, :USERID);",
+            ["CONTENIDO" => $data["CONTENIDO"], "OPCION_UNO" => $this->createOpcion($data["OPCION_UNO"], ($data["RESPUESTA_CORRECTA"] == "1") ? 1 : 0),
+                "OPCION_DOS" => $this->createOpcion($data["OPCION_DOS"], ($data["RESPUESTA_CORRECTA"] == "2") ? 1 : 0),
+                "OPCION_TRES" => $this->createOpcion($data["OPCION_TRES"], ($data["RESPUESTA_CORRECTA"] == "3") ? 1 : 0), "USERID" => $data["USERID"]]);
+    }
+
+    public function createRespuesta(array $data) {
+        return parent::executeSQL("INSERT INTO respuesta(CONTENIDO, USERID, PREGUNTAID)
+                                    VALUES (:CONTENIDO, :USERID, :PREGUNTAID);",
+            ["CONTENIDO" => $data["CONTENIDO"], "USERID" => $data["USERID"], "PREGUNTAID" => $data["PREGUNTAID"]]);
+    }
+
+    public function followUser(array $data) {
+        return parent::executeSQL("INSERT INTO seguidores(USUARIO_QUE_SIGUE, USUARIO_SEGUIDO)
+                                    VALUES (:USUARIO_QUE_SIGUE, :USUARIO_SEGUIDO);",
+            ["USUARIO_QUE_SIGUE" => $data["USUARIO_QUE_SIGUE"], "USUARIO_SEGUIDO" => $data["USUARIO_SEGUIDO"]]);
     }
 }
